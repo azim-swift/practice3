8af4ab2dd0181075ab4fa57e8732cd891719c849
Azim Bayramgeldyev <96699203+azim-swift@users.noreply.github.com>
2024-05-09 20:46:47 +0400

    Update laba3.py

--- laba3.py
+++ laba3.py
@@ -1,63 +1,72 @@
-# С клавиатуры вводится два числа K и N. Квадратная матрица А(N,N), состоящая из 4-х равных по размерам подматриц, 
-# B,C,D,E заполняется случайным образом целыми числами в интервале [-10,10]. Для тестирования использовать не случайное заполнение, а целенаправленное.
-
-# Вариант 7
-# Формируется матрица F следующим образом: если в С количество нулевых элементов в нечетных столбцах в области 4 больше, 
-# чем количество нулевых  элементов в четных столбцах в области 1, то поменять в В симметрично области 2 и 3 местами, иначе С и Е поменять местами несимметрично. 
-# При этом матрица А не меняется. После чего вычисляется выражение: ((F*A)– (K * AT) . Выводятся по мере формирования А, F и все матричные операции последовательно.
+"""ВАРИАНТ 7.Формируется матрица F следующим образом: если в С количество нулевых элементов в нечетных столбцах в области 4 больше
+, чем количество нулевых  элементов в четных столбцах в области 1, то поменять в В симметрично области 2 и 3 местами
+, иначе С и Е поменять местами несимметрично. При этом матрица А не меняется.
+ После чего вычисляется выражение: ((F*A)– (K * AT) . Выводятся по мере формирования А, F и все матричные операции последовательно."""
 
 import random
+sumC4=0
+sumC1=0
+K=int(input("Введите К="))
+N=int(input("Введите N="))
+save=0
+minus = 0
+"""формируем матрици размеров N/2 с случайными значением от -10 до 10"""
+D =  [[random.randint(-10,10 ) for j in range(N//2)] for i in range(N//2)]
+E =  [[random.randint(-10,10 ) for j in range(N//2)] for i in range(N//2)]
+C =  [[random.randint(-10,10 ) for j in range(N//2)] for i in range(N//2)]
+B =  [[random.randint(-10,10 ) for j in range(N//2)] for i in range(N//2)]
+G = [ [0]*((N//2)*2) for i in range((N//2)*2) ]
+H = [ [0]*((N//2)*2) for i in range((N//2)*2) ]
+A = [D[i] + E[i]  for i in range(N//2)] #создаем матрицу А добовляя в нее по элементно матрицы D E C B
+A += [C[i] + B[i] for i in range(N//2)]
+print("матрица A\n",A,"\n\n")
 
-def swap_areas(matrix1, matrix2):
-    temp = matrix1.copy()
-    matrix1 = matrix2.copy()
-    matrix2 = temp
-    return matrix1, matrix2
 
-def calculate_expression(K, N):
-    # Случайным образом заполнение матрицы А [-10, 10]
-    A = [[random.randint(-10, 10) for _ in range(N)] for _ in range(N)]
+"""если в С количество нулевых элементов в нечетных столбцах в области 4 больше
+, чем количество нулевых  элементов в четных столбцах в области 1, то поменять в В симметрично области 2 и 3 местами"""
+for i in range(0,N//2,2): # ищем в С количество нулевых элементов в нечетных столбцах в области 4
+    for j in range(0,N//2):
 
-    # Заполнение подматрицы с интервалом [-10, 10]
-    D = [[random.randint(-10, 10) for _ in range(N // 2)] for _ in range(N // 2)]
-    E = [[random.randint(-10, 10) for _ in range(N // 2)] for _ in range(N // 2)]
-    C = [[random.randint(-10, 10) for _ in range(N // 2)] for _ in range(N // 2)]
-    B = [[random.randint(-10, 10) for _ in range(N // 2)] for _ in range(N // 2)]
+        if j < i and j < ((N//2) - 1) - i and C[j][i]==0:
+            sumC4 += 1
 
 
-    # Проверка на состояние и при необходимсоти замена местами
-    if sum(C[i][j] != 0 for i in range(1, N // 2, 2) for j in range(2)) > sum(C[i][j] != 0 for i in range(0, N // 2, 2) for j in range(N // 2 - 2, N // 2)):
-        B = swap_areas(B, B)  # Swap areas 2 and 3 in B
-    else:
-        C, E = swap_areas(C, E)  # Swap C and E
 
-    # Вычесление матрицы
-    F = [[0] * N for _ in range(N)]
-    for i in range(N // 2):
-        for j in range(N // 2):
-            F[i][j] = B[i][j]
-            F[i][j + N // 2] = C[i][j]
-            F[i + N // 2][j] = D[i][j]
-            F[i + N // 2][j + N // 2] = E[i][j]
+for i in range(1,N//2,2):# ищем количество нулевых  элементов в четных столбцах в области 1
+    for j in range(0,N//2):
 
-    # Вычесление выражения ((F * A) - (K * A transpose))
-    result = [[0] * N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            result[i][j] = sum(F[i][k] * A[k][j] for k in range(N)) - K * A[j][i]
+        if j < i and j > ((N//2) - 1) - i and C[j][i]==0:
+            sumC1 += 1
 
-    # Вывод матрицы с результатом
-    print("Матрица A:")
-    for row in A:
-        print(row)
-    print("Матрица F:")
-    for row in F:
-        print(row)
-    print("Вывод:")
-    for row in result:
-        print(row)
+if sumC4 > sumC1 : # меняем  в В симметрично области 2 и 3 местами
 
-# Тест самой программы
-K = int(input("Введите K: "))
-N = int(input("Ввелите N: "))
-calculate_expression(K, N)
+    for i in range((N//2) - 1, N // 4, -1):
+        minus = 0
+        for j in range(0, N // 2):
+
+            if i > j and i > (N//2) - 1 - j:
+                minus += 1
+                save = C[i][j]
+                C[i][j] = C[i-minus][j-minus]
+                C[i - minus][j - minus] = save
+
+
+    F = [D[i] + E[i]  for i in range(N//2)]
+    F += [C[i] + B[i] for i in range(N//2)]
+
+else:
+    F = [D[i] + C[i]  for i in range(N//2)]
+    F += [E[i] + B[i] for i in range(N//2)]
+
+print(f'\nМатрица F {F}')
+
+for g in range(len(A)):
+    for i in range(len(F[0])):
+        for k in range(len(F)):G[g][i] += F[g][k] * A[k][i]
+print(f'\nF*A  {G}\n')
+for g in range(len(A)):
+    for i in range(len(A[0])):H[i][g]=A[g][i] * K
+print(f'\nAT * K {H}\n',)
+for g in range(len(A)):
+    for i in range(len(A[0])): G[g][i]=G[g][i]-H[g][i]
+print("Конечный результат\n",G,"\n")
