55a2c5c46555ec9a96367929a2aef98f7e106608
Azim Bayramgeldyev <96699203+azim-swift@users.noreply.github.com>
2024-05-18 00:44:20 +0400

    Update laba3.py

--- laba3.py
+++ laba3.py
@@ -4,11 +4,12 @@
  После чего вычисляется выражение: ((F*A)– (K * AT) . Выводятся по мере формирования А, F и все матричные операции последовательно."""
 
 import random
-sumC4=0
+sumC4=999999
 sumC1=0
 K=int(input("Введите К="))
 N=int(input("Введите N="))
 save=0
+wall = -1
 minus = 0
 """формируем матрици размеров N/2 с случайными значением от -10 до 10"""
 D =  [[random.randint(-10,10 ) for j in range(N//2)] for i in range(N//2)]
@@ -24,38 +25,42 @@ print("матрица A\n",A,"\n\n")
 
 """если в С количество нулевых элементов в нечетных столбцах в области 4 больше
 , чем количество нулевых  элементов в четных столбцах в области 1, то поменять в В симметрично области 2 и 3 местами"""
-for i in range(0,N//2,2): # ищем в С количество нулевых элементов в нечетных столбцах в области 4
-    for j in range(0,N//2):
-
-        if j < i and j < ((N//2) - 1) - i and C[j][i]==0:
+for i in range(0,(N//4)-1): # ищем в С количество нулевых элементов в нечетных столбцах в области 4
+    wall += 1
+    for j in range(0+wall , (N//2)-wall ):
+        
+        if i < j  and i < ((N//2) - 1) - j and C[i][j]==0 and j % 2 == 0:
             sumC4 += 1
 
+wall = -1
 
-
-for i in range(1,N//2,2):# ищем количество нулевых  элементов в четных столбцах в области 1
-    for j in range(0,N//2):
-
-        if j < i and j > ((N//2) - 1) - i and C[j][i]==0:
+for i in range((N//2)-1,(N//4)-1,-1):# ищем количество нулевых  элементов в четных столбцах в области 1
+    wall += 1
+    for j in range(1+wall , (N//2)-wall-1 ):
+        
+        if j < i and j > ((N//2) - 1) - i and C[j][i]==0 and  i % 2 != 0:
             sumC1 += 1
+
+wall = -1
 
 if sumC4 > sumC1 : # меняем  в В симметрично области 2 и 3 местами
 
-    for i in range((N//2) - 1, N // 4, -1):
+    for i in range((N // 2) - 1, N // 4, -1):
         minus = 0
-        for j in range(0, N // 2):
+        wall  += 1
+        for j in range(1+wall , (N//2)-wall-1):
 
-            if i > j and i > (N//2) - 1 - j:
+            if i > j and i > (N // 2) - 1 - j:
                 minus += 1
                 save = C[i][j]
-                C[i][j] = C[i-minus][j-minus]
+                C[i][j] = C[i - minus][j - minus]
                 C[i - minus][j - minus] = save
 
-
-    F = [D[i] + E[i]  for i in range(N//2)]
+    F = [D[i] + E[i] for i in range(N//2)]
     F += [C[i] + B[i] for i in range(N//2)]
 
 else:
-    F = [D[i] + C[i]  for i in range(N//2)]
+    F = [D[i] + C[i] for i in range(N//2)]
     F += [E[i] + B[i] for i in range(N//2)]
 
 print(f'\nМатрица F {F}')
@@ -64,9 +69,13 @@ for g in range(len(A)):
     for i in range(len(F[0])):
         for k in range(len(F)):G[g][i] += F[g][k] * A[k][i]
 print(f'\nF*A  {G}\n')
+
+
 for g in range(len(A)):
     for i in range(len(A[0])):H[i][g]=A[g][i] * K
 print(f'\nAT * K {H}\n',)
+
+
 for g in range(len(A)):
     for i in range(len(A[0])): G[g][i]=G[g][i]-H[g][i]
 print("Конечный результат\n",G,"\n")
